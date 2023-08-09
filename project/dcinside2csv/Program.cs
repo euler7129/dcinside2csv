3690798b286272fd064e6eb47ae7849adc1c1503
euler7129 <euler7129@proton.me>
2023-08-09 16:24:43 +0900

    Create project

---
+++ project/dcinside2csv/Program.cs
@@ -0,0 +1,23 @@
+﻿using CsvHelper;
+using System.Globalization;
+
+public class MyCommands : ConsoleAppBase
+{
+	[RootCommand]
+	public void RootCommand(
+		[Option("i", "Directory path containing input HTML files")] string inputDirPath,
+		[Option("p", "Directory path of the output data")] string outputDirPath)
+	{// -i "C:\temp\dcinside\gallery" -p "C:\temp\dcinside\gallery_csv"
+		var arguments = Context.Arguments;
+		Console.WriteLine($"Executing with arguments: \"{string.Join(" ", arguments)}\"");
+		Console.WriteLine("dcinside2csv has been started.");
+
+		// Each HTML file will generate BlogPost, Comment, and Image objects.
+		dcinside2csv(inputDirPath, outputDirPath);
+	}
+
+	private void dcinside2csv(string inputDirPath, string outputDirPath)
+	{
+		throw new NotImplementedException();
+	}
+}
