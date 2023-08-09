c6b7907216f59d54aad6ab73721320a8dc73a656
euler7129 <euler7129@proton.me>
2023-08-09 17:40:01 +0900

    (WIP) Extracting HTML→Gonna save & replace images

--- project/dcinside2csv/Program.cs
+++ project/dcinside2csv/Program.cs
@@ -1,5 +1,9 @@
-﻿using CsvHelper;
+﻿using AngleSharp.Html.Parser;
+using AngleSharp.XPath;
+using CsvHelper;
 using System.Globalization;
+
+ConsoleApp.Run<MyCommands>(args);
 
 public class MyCommands : ConsoleAppBase
 {
@@ -7,7 +11,7 @@ public class MyCommands : ConsoleAppBase
 	public void RootCommand(
 		[Option("i", "Directory path containing input HTML files")] string inputDirPath,
 		[Option("p", "Directory path of the output data")] string outputDirPath)
-	{// -i "C:\temp\dcinside\gallery" -p "C:\temp\dcinside\gallery_csv"
+	{// -i "D:\temp\dcinside\gallery" -p "D:\temp\dcinside\gallery_csv"
 		var arguments = Context.Arguments;
 		Console.WriteLine($"Executing with arguments: \"{string.Join(" ", arguments)}\"");
 		Console.WriteLine("dcinside2csv has been started.");
@@ -18,6 +22,39 @@ public class MyCommands : ConsoleAppBase
 
 	private void dcinside2csv(string inputDirPath, string outputDirPath)
 	{
-		throw new NotImplementedException();
+		// Get all HTML files in the input directory.
+		var htmlFiles = Directory.GetFiles(inputDirPath, "*.html", SearchOption.AllDirectories);
+		Console.WriteLine($"Found {htmlFiles.Length} HTML files.");
+		// Using AngleSharp to parse HTML files.
+		var parser = new HtmlParser();
+		// Using CsvHelper to write CSV files.
+		foreach ( var htmlFile in htmlFiles )
+		{
+			Console.WriteLine($"Parsing {htmlFile}...");
+			var html = File.ReadAllText(htmlFile);
+			var document = parser.ParseDocument(html);
+			// Narrow down the scope to the article node.
+			//var articleNode = document.Body.SelectSingleNode("//*[@id=\"container\"]/section/article[2]");
+
+			// Get element by class name
+			var category = document.QuerySelector(".title_headtext").TextContent;
+			var subject = document.QuerySelector(".title_subject").TextContent;
+			var author = document.Body.SelectSingleNode("//*[@id=\"container\"]/section/article[2]/div[1]/header/div/div/div[1]/span[1]/a/em").TextContent;
+			// Get write_div div element
+			var writeDiv = document.QuerySelector("div.write_div");
+			// Filter only HtmlDivElement elements in writeDiv
+			var divs = writeDiv.Children.Where(x => x is AngleSharp.Html.Dom.IHtmlDivElement).Cast<AngleSharp.Html.Dom.IHtmlDivElement>();
+
+			// Save each div element's InnerHtml to a text file
+			int index = 0;
+			foreach ( var div in divs )
+			{
+				var divHtml = div.InnerHtml;
+				var divHtmlFilePath = Path.Combine(outputDirPath, $"{div.Id}{index++}.html");
+				File.WriteAllText(divHtmlFilePath, divHtml);
+			}
+
+			break;
+		}
 	}
 }
